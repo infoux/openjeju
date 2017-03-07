6986bd38c43369f0a78a91013337ce5ded9d8159
rhkanrdl <emrdl7@gmail.com>
2017-03-07 11:00:46 +0900

    메인 코딩중

--- js/common.js
+++ js/common.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    $(".menuToggle").click(function () {
+        $("#header nav").addClass("open");
+    });
 
-
+    $("#header nav .close").click(function () {
+        $("#header nav").removeClass("open");
+    });
 
 });
-
