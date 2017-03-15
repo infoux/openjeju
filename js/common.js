456b0497dc7b33125f90caaa2b6c359ce49b5f1f
rhkanrdl <emrdl7@gmail.com>
2017-03-15 11:19:01 +0900

    테마리스트 추가

--- js/common.js
+++ js/common.js
@@ -1,42 +1,68 @@
-$(document).ready(function () {
-    var mainFood = null;
-    var mainFood_config = {
+$(document).ready(function() {
+
+
+
+    var carouselWidth = 1190;
+    var ttCarousel;
+    var ttCarousel_Config = {
         auto: true,
         moveSlides: 1,
-        infiniteLoop: true
+        infiniteLoop: true,
+        preventDefaultSwipeY: false
+    }
+
+    if ($(window).width() < carouselWidth) {
+        if (ttCarousel == null)
+            ttCarousel = $('.main-food .slider').bxSlider(ttCarousel_Config);
     };
+
+    $(window).resize(function() {
+        try {
+            if ($(window).width() < carouselWidth) {
+                if (ttCarousel) {
+                    ttCarousel.reloadSlider();
+                } else {
+                    ttCarousel = $('.main-food .slider').bxSlider(ttCarousel_Config);
+                }
+            } else {
+                if (ttCarousel) {
+                    ttCarousel.destroySlider();
+                    $('.main-food .slider').each(function() {
+                        $(this).find("li").css("width", "");
+                        $(this).attr("style", "");
+                    });
+                }
+            }
+        } catch (e) {
+
+        }
+    });
+
 
 
     function viewMode() {
         var windowWidth = $(window).width();
-
         if (windowWidth < 1199) {
             $("body").removeClass("pc");
             $("body").addClass("mobile");
-            if (mainFood == null) {
-                mainFood = $('.main-food ul').bxSlider(mainFood_config);
-            }
+
         } else {
 
             $("body").addClass("pc");
             $("body").removeClass("mobile");
-            if (mainFood != null) {
-                mainFood.destroySlider();
-                mainFood = null;
 
-                $('.main-food ul').attr("style", "");
-                $('.main-food li').attr("style") = "";
-            }
 
         }
 
         $(".main-issue").css("background", "url(" + $(".main-issue .issue-bg").attr("src") + ") no-repeat center / cover");
-        $(".main-food li").each(function () {
+        $(".sub-head").css("background", "url(" + $(".sub-head>img").attr("src") + ") no-repeat center / cover");
+        $(".main-food li").each(function() {
             $(this).css("background", "url(" + $(this).find("img").attr("src") + ") no-repeat center / cover");
         });
 
 
-        var mag = ($(window).width() - 330) / 2;
+        var mag = ($(window).width() - $('.mobile .main-food li').width() - 20) / 2;
+
         $('.mobile .main-food ul').css('margin-left', mag).css('margin-right', mag);
 
     }
@@ -52,31 +78,47 @@ $(document).ready(function () {
     viewMode();
     mainHeight();
 
-    $(".menuToggle").click(function () {
+    $(".menuToggle").click(function() {
         $("#header nav").addClass("open");
     });
-    $("#header nav .close").click(function () {
+    $("#header nav .close").click(function() {
         $("#header nav").removeClass("open");
     });
 
+    $(".indicator li a").click(function() {
+        $(".indicator li .sub-menu").css("display", "none");
+        $(this).parent().find(".sub-menu").toggle();
+    });
 
 
+    $('#map-area li a').on('click', function() {
+        var activeTab = $(this).parent().attr('id');
+        $('#map-area li').removeClass('active');
+        $(this).parent().addClass('active');
+
+        $('#map-area').addClass(activeTab);
+        $('#map-area .map-pop').css('display', 'none');
+        $('#map-area .map-pop.' + activeTab).css('display', 'block');
+        console.log(activeTab);
+    });
 
 
-    $(window).resize(function () {
+    $(window).resize(function() {
         viewMode();
         mainHeight();
 
+
     });
 
-    $(window).load(function () {
-
-        var snsGird = new Masonry('.main-sns .grid', {
-            // options
-            itemSelector: '.grid-item',
-            gutter: 10
-        });
-    })
+    $(window).load(function() {
+        if ($("#content").hasClass("main")) {
+            var snsGird = new Masonry('.main-sns .grid', {
+                // options
+                itemSelector: '.grid-item',
+                gutter: 10
+            });
+        }
+    });
 
 
 });
