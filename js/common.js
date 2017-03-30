443b325c999e9cf3a4692e920a32bba75bab4e00
method <method@infomind.co.kr>
2017-03-30 23:25:50 +0900

    메인 테마 / 게시판 쓰기 보기 리스트 추가

--- js/common.js
+++ js/common.js
@@ -52,6 +52,7 @@ $(document).ready(function() {
 
         $(".main-issue").css("background", "url(" + $(".main-issue .issue-bg").attr("src") + ") no-repeat center / cover");
         $(".sub-head").css("background", "url(" + $(".sub-head>img").attr("src") + ") no-repeat center / cover");
+        $(".visual-area").css("background", "url(" + $(".visual-area img").attr("src") + ") no-repeat center / cover");
         $(".pc .view-head").css("background", "url(" + $(".view-head img").attr("src") + ") no-repeat center / cover");
         $(".mobile .view-head").css("background", "");
         $(".mobile .view-head .image").css("background", "url(" + $(".view-head img").attr("src") + ") no-repeat center / cover");
@@ -68,7 +69,9 @@ $(document).ready(function() {
 
     function mainHeight() {
         $(".pc .main-issue").css("height", $(window).height() - 70);
+        $(".pc .visual-area").css("height", $(window).height());
         $(".mobile .main-issue").css("height", "");
+        $(".mobile .visual-area").css("height", "");
     }
 
 
